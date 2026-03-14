3f2840349f1dd2c7d36cdc9b17aac16d7366119a
Rodolfo Fuentealba <rodfuentealba@Rodolfos-MacBook-Air.local>
2026-03-14 13:00:55 -0300

    replaceIconFooter

--- src/components/sections/FooterSection.tsx
+++ src/components/sections/FooterSection.tsx
@@ -1,5 +1,5 @@
 import titleDB from "@/assets/titleDB.svg";
-import { Heart } from "lucide-react";
+import { HeartHandshake } from "lucide-react";
 import { useLanguage } from "@/hooks/useLanguage";
 
 const FooterSection = () => {
@@ -7,7 +7,7 @@ const FooterSection = () => {
 
   return (
     <footer className="bg-footer-red py-12 px-6 text-center">
-      <Heart className="w-10 h-10 text-hero-navy-foreground mx-auto mb-4 fill-hero-navy-foreground" />
+      <HeartHandshake className="w-10 h-10 mx-auto mb-4" color="#ffffff"/>
       <img src={titleDB} alt="Damiam & Benedetta" className="h-6 mx-auto opacity-90" />
       <p className="text-hero-navy-foreground/60 text-sm font-body mt-4">
         {t("footer.date")}
