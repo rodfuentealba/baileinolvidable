007e392a2525abf7de093593434da3cab4c7d4b5
Rodolfo Fuentealba <rodfuentealba@Rodolfos-MacBook-Air.local>
2026-03-19 16:37:12 -0300

    addAnalyticOK

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/sections/HeroSection";
 import DescriptionSection from "@/components/sections/DescriptionSection";
